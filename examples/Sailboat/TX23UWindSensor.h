df920bf101480fd5f0b507a6ebda46b9dea44e62
Pavel Bobov <pbobov@gmail.com>
2016-06-02 22:32:32 -0700

    #26 Optimized TX23UWindSensor implementation

--- examples/Sailboat/TX23UWindSensor.h
+++ examples/Sailboat/TX23UWindSensor.h
@@ -1,8 +1,21 @@
 /*
  * TX23UWindSensor.h
  *
- * Created on: May 27, 2016
- *     Author: pavel
+ * Driver for LaCrosse TX-23U wind sensor
+ *
+ * (C) Copyright 2016 Pavel Bobov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef TX23UWINDSENSOR_H_
